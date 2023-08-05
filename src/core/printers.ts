c40252a14c87742683ddc80701650c5c76366c79
fliseno1k <fliseno1k@gmail.com>
2023-08-06 06:08:44 +1000

    fix month printing function

--- src/core/printers.ts
+++ src/core/printers.ts
@@ -1,7 +1,7 @@
 import { Atom } from './atom';
 
 const WEEK_LENGTH = 7;
-const EMPTY_CELL = '00';
+const EMPTY_CELL = '  ';
 
 function emtpyWeek(): string[] {
 	return new Array(WEEK_LENGTH).fill(EMPTY_CELL);
@@ -11,25 +11,26 @@ function toValue(atom: Atom): string {
 	return atom.date.getDate().toString().padStart(2, '0');
 }
 
-function printMonth(month: Atom[]): void {
+export function printMonth(month: Atom[], firstDayIndex: number = 0): void {
 	if (month.length === 0) {
 		throw new Error('Provide none empty month');
 	}
 
-	const firstDay = month[0].date.getDay() - 1;
+	let firstDay = month[0].date.getDay() - firstDayIndex;
+	firstDay += firstDay < 0 ? 7 : 0;
 
-	for (let i = -firstDay; i < month.length + firstDay; i += WEEK_LENGTH) {
+	for (let i = -firstDay; i < month.length + 1; i += WEEK_LENGTH) {
 		const start = Math.max(i, 0);
 		const end = Math.min(i + WEEK_LENGTH - 1, month.length - 1);
 		const days = month.slice(start, end + 1).map(toValue);
 
 		let outputString = '';
 
-		if (days.length === 7) {
+		if (days.length === WEEK_LENGTH) {
 			outputString = days.join(' ');
 		} else {
 			const week = emtpyWeek();
-			week.splice((start + 1) % 7, days.length, ...days);
+			week.splice((start + firstDay) % WEEK_LENGTH, days.length, ...days);
 
 			outputString = week.join(' ');
 		}
